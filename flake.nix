bf42f2fbfa8fa1fb0818ac232c350b7c670de2fd
stoutpanda <stoutpanda@protonmail.com>
2025-07-16 22:53:22 -0500

    lix bug -- pointing flake to main -- need to roll back after next release.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Nixos config flake for StoutPanda";
-  
+
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
@@ -11,13 +11,13 @@
       "chaotic-nyx.cachix.org-1:HfnXSw4j04dMLJBJG7yDYPdFCGAtOMjXQWqA7BsnhRg="
     ];
   };
-  
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    #lix import 
+    #lix import
     lix-module = {
-        url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     #point everything to my home-manager flake
     home-manager-config.url = "github:stoutpanda/home-manager";
@@ -25,34 +25,47 @@
     #chotic nyx!
     chaotic.follows = "home-manager-config/chaotic";
     #nixos hardware
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";         
-    agenix.follows = "home-manager-config/agenix"; 
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    agenix.follows = "home-manager-config/agenix";
   };
-  outputs = { self, nixpkgs, lix-module,  nixos-hardware, home-manager, home-manager-config, chaotic, agenix, ... }@inputs: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    nixosConfigurations.nixos-whitedwarf = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        lix-module.nixosModules.default
-        { nixpkgs.overlays = [ chaotic.overlays.default ]; }
-	agenix.nixosModules.default
-        ./configuration.nix 
-        ./hosts/nixos-whitedwarf/configuration.nix
-         
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      lix-module,
+      nixos-hardware,
+      home-manager,
+      home-manager-config,
+      chaotic,
+      agenix,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.nixos-whitedwarf = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          lix-module.nixosModules.default
+          { nixpkgs.overlays = [ chaotic.overlays.default ]; }
+          agenix.nixosModules.default
+          ./configuration.nix
+          ./hosts/nixos-whitedwarf/configuration.nix
+
+        ];
+      };
+
+      # Formatter for nix files
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      # Checks to run
+      checks.${system} = {
+        format = pkgs.runCommand "check-format" { } ''
+          ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
+          touch $out
+        '';
+      };
     };
-    
-    # Formatter for nix files
-    formatter.${system} = pkgs.nixpkgs-fmt;
-    
-    # Checks to run
-    checks.${system} = {
-      format = pkgs.runCommand "check-format" {} ''
-        ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
-        touch $out
-      '';
-    };
-};
 }
